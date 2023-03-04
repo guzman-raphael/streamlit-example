73e09287328acaba797bd70303791b98aa3f943b
Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>
2023-03-04 16:40:31 -0600

    Add auth.

--- private_app.py
+++ private_app.py
@@ -1,7 +1,32 @@
 # https://guzman-raphael-streamlit-example-private-app-qhzxwd.streamlit.app/
 
-import streamlit as st
-import datajoint as dj
-from streamlit_keycloak import login
+# import streamlit as st
+# import datajoint as dj
+# from streamlit_keycloak import login
 
-st.title('Hi raph!')
+# st.title('Hi raph!')
+
+
+
+from dataclasses import asdict
+from streamlit_keycloak import login
+import streamlit as st
+
+
+def main():
+    st.subheader(f"Welcome {keycloak.user_info['preferred_username']}!")
+    st.write(f"Here is your user information:")
+    st.write(asdict(keycloak))
+    if st.button("Disconnect"):
+        keycloak.authenticated = False
+
+
+st.title("Streamlit Keycloak example")
+keycloak = login(
+    url="https://accounts.datajoint.com",
+    realm="master",
+    client_id="works",
+)
+
+if keycloak.authenticated:
+    main()
