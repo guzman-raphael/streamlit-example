f9ad0c90669abbfb6ac3c46000e7e67d7dc85978
Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>
2023-03-04 16:34:52 -0600

    Add new dependencies.

--- private_app.py
+++ private_app.py
@@ -1,5 +1,7 @@
 # https://guzman-raphael-streamlit-example-private-app-qhzxwd.streamlit.app/
 
 import streamlit as st
+import datajoint as dj
+from streamlit_keycloak import login
 
 st.title('Hi raph!')
