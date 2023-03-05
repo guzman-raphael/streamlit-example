aaeeeb1d7dd1c0e2fa162ea8d07f6cf9d55a99e6
Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>
2023-03-05 13:16:54 -0600

    Display user name.

--- private_app.py
+++ private_app.py
@@ -1,13 +1,5 @@
 # https://guzman-raphael-streamlit-example-private-app-qhzxwd.streamlit.app/
 
-# import streamlit as st
-# import datajoint as dj
-# from streamlit_keycloak import login
-
-# st.title('Hi raph!')
-
-
-
 import streamlit as st
 import streamlit_keycloak
 import datajoint as dj
@@ -18,9 +10,6 @@ def get_connection():
         url="https://keycloak.dev.datajoint.io",
         realm="master",
         client_id='my-client',
-        # init_options={
-        #     "checkLoginIframe": False
-        # },
     )
     if keycloak.authenticated:
         return dj.Connection(
@@ -31,29 +20,5 @@ def get_connection():
 
 conn = get_connection()
 
-st.title("user's schemas:")
+st.title(f"{conn.conn_info['user']}'s schemas:")
 st.write(dj.list_schemas(connection=conn))
-
-
-# def main():
-#     st.subheader(f"Welcome {keycloak.user_info['preferred_username']}!")
-#     conn = dj.Connection(host=st.secrets["database_server"], user=keycloak.user_info['preferred_username'], password=keycloak.access_token)
-#     st.write(dj.list_schemas(connection=conn))
-#     st.write(f"Here is your user information:")
-#     st.write(asdict(keycloak))
-#     if st.button("Disconnect"):
-#         keycloak.authenticated = False
-
-
-# st.title("Streamlit Keycloak example")
-# keycloak = login(
-#     url="https://keycloak.dev.datajoint.io",
-#     realm="master",
-#     client_id='my-client',
-#     init_options={
-#         "checkLoginIframe": False
-#     },
-# )
-
-# if keycloak.authenticated:
-#     main()
