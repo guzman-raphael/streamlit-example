a4cf3afb0328fd773772c9b8e3d1498c3f5ff956
Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>
2023-03-05 13:52:04 -0600

    Make auth more modular.

--- private_app.py
+++ private_app.py
@@ -5,22 +5,20 @@ import streamlit_keycloak
 import datajoint as dj
 
 
-def get_connection():
-    if (host:=st.experimental_get_query_params().get("database_host")):
-        keycloak = streamlit_keycloak.login(
-            url="https://keycloak.dev.datajoint.io",
-            realm="master",
-            client_id='my-client',
+def get_connection(**kwargs):
+    if (host:=st.experimental_get_query_params().get("database_host")) and (
+        keycloak:=streamlit_keycloak.login(**kwargs)
+    ).authenticated:
+        return dj.Connection(
+            host=host[0],
+            user=keycloak.user_info["preferred_username"],
+            password=keycloak.access_token,
         )
-        if keycloak.authenticated:
-            return dj.Connection(
-                host=host[0],
-                user=keycloak.user_info['preferred_username'],
-                password=keycloak.access_token
-            )
 
-conn = get_connection()
-if conn:
+
+if (conn:=get_connection(
+    url="https://keycloak.dev.datajoint.io", realm="master", client_id="my-client"
+)):
     st.write(f"{conn.conn_info['user']}'s schemas:")
     st.write(dj.list_schemas(connection=conn))
 else:
