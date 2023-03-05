cbe7b85e37073b2efdf511b7ae7ac7f141340e71
Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>
2023-03-05 13:13:05 -0600

    Simplify auth logic.

--- private_app.py
+++ private_app.py
@@ -8,31 +8,51 @@
 
 
 
-from dataclasses import asdict
-from streamlit_keycloak import login
 import streamlit as st
+import streamlit_keycloak
 import datajoint as dj
 
 
-def main():
-    st.subheader(f"Welcome {keycloak.user_info['preferred_username']}!")
-    conn = dj.Connection(host=st.secrets["database_server"], user=keycloak.user_info['preferred_username'], password=keycloak.access_token)
-    st.write(dj.list_schemas(connection=conn))
-    st.write(f"Here is your user information:")
-    st.write(asdict(keycloak))
-    if st.button("Disconnect"):
-        keycloak.authenticated = False
+def get_connection():
+    keycloak = streamlit_keycloak.login(
+        url="https://keycloak.dev.datajoint.io",
+        realm="master",
+        client_id='my-client',
+        init_options={
+            "checkLoginIframe": False
+        },
+    )
+    if keycloak.authenticated:
+        return dj.Connection(
+            host=st.secrets["database_server"],
+            user=keycloak.user_info['preferred_username'],
+            password=keycloak.access_token
+        )
+
+conn = get_connection()
+
+st.write(dj.list_schemas(connection=conn))
 
 
-st.title("Streamlit Keycloak example")
-keycloak = login(
-    url="https://keycloak.dev.datajoint.io",
-    realm="master",
-    client_id='my-client',
-    init_options={
-        "checkLoginIframe": False
-    },
-)
+# def main():
+#     st.subheader(f"Welcome {keycloak.user_info['preferred_username']}!")
+#     conn = dj.Connection(host=st.secrets["database_server"], user=keycloak.user_info['preferred_username'], password=keycloak.access_token)
+#     st.write(dj.list_schemas(connection=conn))
+#     st.write(f"Here is your user information:")
+#     st.write(asdict(keycloak))
+#     if st.button("Disconnect"):
+#         keycloak.authenticated = False
 
-if keycloak.authenticated:
-    main()
+
+# st.title("Streamlit Keycloak example")
+# keycloak = login(
+#     url="https://keycloak.dev.datajoint.io",
+#     realm="master",
+#     client_id='my-client',
+#     init_options={
+#         "checkLoginIframe": False
+#     },
+# )
+
+# if keycloak.authenticated:
+#     main()
