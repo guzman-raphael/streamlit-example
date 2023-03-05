0c6829f46ff72937fe04d58071c12ad07242923f
Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>
2023-03-05 13:38:43 -0600

    Improve error handling.

--- private_app.py
+++ private_app.py
@@ -6,22 +6,22 @@ import datajoint as dj
 
 
 def get_connection():
-    keycloak = streamlit_keycloak.login(
-        url="https://keycloak.dev.datajoint.io",
-        realm="master",
-        client_id='my-client',
-    )
-    host = st.experimental_get_query_params().get("database_host", [None])[0]
-    if keycloak.authenticated and host:
-        return dj.Connection(
-            host=host,
-            user=keycloak.user_info['preferred_username'],
-            password=keycloak.access_token
+    if (host:=st.experimental_get_query_params().get("database_host")):
+        keycloak = streamlit_keycloak.login(
+            url="https://keycloak.dev.datajoint.io",
+            realm="master",
+            client_id='my-client',
         )
+        if keycloak.authenticated:
+            return dj.Connection(
+                host=host[0],
+                user=keycloak.user_info['preferred_username'],
+                password=keycloak.access_token
+            )
 
 conn = get_connection()
 if conn:
     st.write(f"{conn.conn_info['user']}'s schemas:")
     st.write(dj.list_schemas(connection=conn))
 else:
-    st.write("Not ready")
+    st.write("Incomplete credentials")
